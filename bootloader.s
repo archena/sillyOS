0b513c230694b059cade9f0002834bf4c9df3a53
Archena <archena>
2014-10-13 23:26:14 +0100

    Stack

--- bootloader.s
+++ bootloader.s
@@ -1,11 +1,20 @@
+;;; A bootloader for a silly little operating system
         [org 0x7c00]
+
+        ;; Set up the stack       
+        mov bp, 0x8000
+        mov sp, bp
+
+        ;; Startup message
         mov bx, message
         call printstring
-        
+
+        ;; Loop forever to stop the CPU executing things that it shouldn't!
         jmp $
 	%include "utils.s"
 message:
         db "sillyOS bootloader!",0
 
+        ;; A bootsector must be 512 bytes, so fill remaining bytes with 0s and end with the magic number
         times 510-($-$$) db 0
         dw 0xaa55
